918cd5ab8ae104cfab1128a3c906eea70ddee318
Robin Johansson <robin.frome@gmail.com>
2025-08-03 11:14:16 +0200

    Removed skeleton example

--- src/fxcore.c
+++ src/fxcore.c
@@ -1,7 +1,7 @@
 /* fxcore extension for PHP */
 
 #ifdef HAVE_CONFIG_H
-# include "config.h"
+	# include "config.h"
 #endif
 
 #include "php.h"
@@ -11,37 +11,10 @@
 
 /* For compatibility with older PHP versions */
 #ifndef ZEND_PARSE_PARAMETERS_NONE
-#define ZEND_PARSE_PARAMETERS_NONE() \
-	ZEND_PARSE_PARAMETERS_START(0, 0) \
-	ZEND_PARSE_PARAMETERS_END()
+	#define ZEND_PARSE_PARAMETERS_NONE() \
+		ZEND_PARSE_PARAMETERS_START(0, 0) \
+		ZEND_PARSE_PARAMETERS_END()
 #endif
-
-/* {{{ void test1() */
-PHP_FUNCTION(test1)
-{
-	ZEND_PARSE_PARAMETERS_NONE();
-
-	php_printf("The extension %s is loaded and working!\r\n", "fxcore");
-}
-/* }}} */
-
-/* {{{ string test2( [ string $var ] ) */
-PHP_FUNCTION(test2)
-{
-	char *var = "World";
-	size_t var_len = sizeof("World") - 1;
-	zend_string *retval;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
-	ZEND_PARSE_PARAMETERS_END();
-
-	retval = strpprintf(0, "Hello %s", var);
-
-	RETURN_STR(retval);
-}
-/* }}}*/
 
 /* {{{ PHP_RINIT_FUNCTION */
 PHP_RINIT_FUNCTION(fxcore)
@@ -66,21 +39,21 @@ PHP_MINFO_FUNCTION(fxcore)
 /* {{{ fxcore_module_entry */
 zend_module_entry fxcore_module_entry = {
 	STANDARD_MODULE_HEADER,
-	"fxcore",					/* Extension name */
-	ext_functions,					/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
-	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
-	PHP_RINIT(fxcore),			/* PHP_RINIT - Request initialization */
-	NULL,							/* PHP_RSHUTDOWN - Request shutdown */
-	PHP_MINFO(fxcore),			/* PHP_MINFO - Module info */
+	"fxcore",				/* Extension name */
+	NULL,					/* zend_function_entry */
+	NULL,					/* PHP_MINIT - Module initialization */
+	NULL,					/* PHP_MSHUTDOWN - Module shutdown */
+	PHP_RINIT(fxcore),		/* PHP_RINIT - Request initialization */
+	NULL,					/* PHP_RSHUTDOWN - Request shutdown */
+	PHP_MINFO(fxcore),		/* PHP_MINFO - Module info */
 	PHP_FXCORE_VERSION,		/* Version */
 	STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
 #ifdef COMPILE_DL_FXCORE
-# ifdef ZTS
-ZEND_TSRMLS_CACHE_DEFINE()
-# endif
-ZEND_GET_MODULE(fxcore)
+	# ifdef ZTS
+		ZEND_TSRMLS_CACHE_DEFINE()
+	# endif
+	ZEND_GET_MODULE(fxcore)
 #endif
