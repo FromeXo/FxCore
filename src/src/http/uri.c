78e3f7b39d2e56843f98fb7112328935db183c45
Robin Johansson <robin.frome@gmail.com>
2025-08-04 15:28:54 +0200

    Add FxCore\Http\Uri methods definitions

---
+++ src/src/http/uri.c
@@ -0,0 +1,121 @@
+#include "php.h"
+
+
+/* {{{ __construct */
+ZEND_METHOD(FxCore_Http_Uri, __construct)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getScheme */
+ZEND_METHOD(FxCore_Http_Uri, getScheme)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getAuthority */
+ZEND_METHOD(FxCore_Http_Uri, getAuthority)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getUserInfo */
+ZEND_METHOD(FxCore_Http_Uri, getUserInfo)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getHost */
+ZEND_METHOD(FxCore_Http_Uri, getHost)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getPort */
+ZEND_METHOD(FxCore_Http_Uri, getPort)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getPath */
+ZEND_METHOD(FxCore_Http_Uri, getPath)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getQuery */
+ZEND_METHOD(FxCore_Http_Uri, getQuery)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ getFragment */
+ZEND_METHOD(FxCore_Http_Uri, getFragment)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withScheme */
+ZEND_METHOD(FxCore_Http_Uri, withScheme)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withUserInfo */
+ZEND_METHOD(FxCore_Http_Uri, withUserInfo)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withHost */
+ZEND_METHOD(FxCore_Http_Uri, withHost)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withPort */
+ZEND_METHOD(FxCore_Http_Uri, withPort)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withPath */
+ZEND_METHOD(FxCore_Http_Uri, withPath)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withQuery */
+ZEND_METHOD(FxCore_Http_Uri, withQuery)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ withFragment */
+ZEND_METHOD(FxCore_Http_Uri, withFragment)
+{
+    return;
+}
+/* }}} */
+
+/* {{{ __toString */
+ZEND_METHOD(FxCore_Http_Uri, __toString)
+{
+    return;
+}
+/* }}} */
