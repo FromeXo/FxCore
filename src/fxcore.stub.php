a9e03214f6b0ace221aa9109c165eb19586af456
Robin Johansson <robin.frome@gmail.com>
2025-08-04 15:24:04 +0200

    Updated the stub file with FxCore\Http\Uri methods

--- src/fxcore.stub.php
+++ src/fxcore.stub.php
@@ -9,6 +9,22 @@ namespace FxCore\Http
 {
     class Uri
     {
-
+        public function __construct() {}
+        public function getScheme(): void {}
+        public function getAuthority(): void {}
+        public function getUserInfo(): void {}
+        public function getHost(): void {}
+        public function getPort(): void {}
+        public function getPath(): void {}
+        public function getQuery(): void {}
+        public function getFragment(): void {}
+        public function withScheme(): void {}
+        public function withUserInfo(): void {}
+        public function withHost(): void {}
+        public function withPort(): void {}
+        public function withPath(): void {}
+        public function withQuery(): void {}
+        public function withFragment(): void {}
+        public function __toString(): string {}
     }
 }
